6f7aa5c4ad64f44da5cd7c247e783c1d35f64e72
JaneNeave <janeneave101@gmail.com>
2023-12-08 16:52:49 +0000

    updated question 1 B

--- HW_3/question1.py
+++ HW_3/question1.py
@@ -11,9 +11,11 @@
 
 from collections import deque
 import timeit
+from typing import List
 
 
 def get_final_queue_order(input_file):
+    # Using a deque (Doubly ended queue) to "append" (& "appendleft") from both ends of the container.
     queue = deque()
     try:
         with open(input_file, 'r') as text_file:
@@ -27,60 +29,60 @@ def get_final_queue_order(input_file):
                 elif command == 'JOIN':
                     queue.append(name)
     except FileNotFoundError:
-        print(f"File not found: {input_file}")
+        print(f'File not found: {input_file}')
     except Exception as e:
-        print(f"An unexpected error occurred: {e}")
+        print(f'An unexpected error occurred: {e}')
     else:
         return list(queue)
 
 
 def test_time_complexity():
-    setup_code = "from question1 import get_final_queue_order"
-    test_code = "get_final_queue_order('hw3_q1.txt')"
-    time_taken = timeit.timeit(test_code, setup=setup_code, number=1000)  # Adjust the number of iterations as needed
-    print(f"Time taken: {time_taken} seconds")
+    setup_code = 'from question1 import get_final_queue_order'
+    test_code = 'get_final_queue_order("hw3_q1.txt")'
+    time_taken = timeit.timeit(test_code, setup=setup_code, number=1000)
+    print(f'Time taken: {time_taken} seconds')
 
 
 if __name__ == '__main__':
     print(get_final_queue_order('hw3_q1.txt'))
     test_time_complexity()
 
-# note: or go through list and append all the joins, append all the jumps and concatenate the lists
+# note:or go through list and append all the joins, append all the jumps and concatenate the lists/work frm middle out
 
 # [B] What is the time and space complexity of your solution? If you are making
 # any assumptions, list them.
 
 # TIME AND SPACE COMPLEXITY:
 # Time Complexity:
-# The time complexity of this solution is O(N), where N is the number of lines in the input file.
-# This is because the algorithm processes each line once, and the operations performed within
-# each iteration (such as appending or appending to the left of the deque) are constant time.
-#
+# For this function the time complexity is primarily determined by the for loop that iterates
+# through each line of the input_file.
+# .strip() and .split() are constant time operations.
+# It is the append() and appendleft() methods which contribute most to the time complexity.
+# Using a deque() provides a O(1) time complexity for append() and appendleft(), as they both take constant time.
+# (Unlike if they were used in a list which would provide O(N) complexity).
+# Nevertheless, the loop iterates through each line in the input_file once, therefore the overall time complexity
+# is O(N), whereby N is the number of lines in the file, and append operations are performed once within each iteration
+# and are constant time.
+
 # Space Complexity:
-# The space complexity is also O(N), where N is the number of lines in the input file.
-# The main contributor to space complexity is the deque, which stores the names of people
-# in the queue. In the worst case, the deque could grow to accommodate all the names from
-# the input file. The list conversion at the end doesn't significantly affect the overall
-# space complexity, as it creates a new list with the same elements.
-#
+# This is determined by the additional memory used by my solution (excluding input space).
+# The main contributor to space complexity is the deque, which is the primary data structure which
+# stores the names of people in the queue. In the worst case, the deque could grow to accommodate all the names from
+# the input file. It's space complixity is O(N), where N is the number of names in the queue.
+# Other variables, such as line, command and name contribute space O(1).
+# The list conversion at the end doesn't significantly affect the overall space complexity,
+# as it creates a new list with the same elements of the deque. It has a space complexity of O(N) because the new
+# list created is of size N.
+# Thus, for my solution the overall space complexity is O(N), whereby N is the number of names in the queue.
+
 # In summary, the provided solution has a linear time complexity O(N) and a linear space
 # complexity O(N), where N is the number of lines in the input file.
 
-
 # ASSUMPTIONS:
-# Input File Format: I am assuming the file follows the stated format of the question,
+# I am assuming the input_file follows the stated format of the question,
 # i.e. 'The first word on each line will either be “JUMP” or “JOIN” and the second word the
 # name of the person', and that each word is separated by a space (' '). I am also assuming
 # that the file only contains the commands 'JUMP' or 'JOIN', otherwise it will raise an
-# error.
-#
-# Command Handling Assumption: The code assumes that the only valid commands are "JUMP" and "JOIN,"
-# and it correctly processes these commands to update the queue.
-#
-# File Existence Assumption: The code assumes that the input file (hw3_q1.txt) exists in the same
-# directory as the script.
-#
-# Valid Input Data Assumption: The code assumes that the input data adheres to the described rules,
-# and it does not perform extensive error checking for invalid or malformed input.
-#
-# Assuming that the file contains no blank lines
+# error. Additionally, I am assuming that the file is not empty and contains text.
+# Assuming that the file exists, and that the function will handle a FileNotFoundError.
+# Assuming that function handles unexpected errors during file processing with the Exception.
